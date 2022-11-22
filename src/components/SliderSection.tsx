acf2c3577b8fca4f5248e4917a2c849521accafd
조현일 <himzei@gmail.com>
2022-11-22 17:28:52 +0900

    aa

--- src/components/SliderSection.tsx
+++ src/components/SliderSection.tsx
@@ -71,7 +71,7 @@ export default function SliderSection() {
             justifyContent="center"
             color="white"
             backgroundImage={
-              "url('https://media.contentapi.ea.com/content/dam/news/www-ea/common/ea-motive-new-title-teaser-16x9.jpg.adapt.1456w.jpg')"
+              "url('https://cdn.britannica.com/60/182360-050-CD8878D6/Avengers-Age-of-Ultron-Joss-Whedon.jpg')"
             }
             backgroundPosition="center"
             backgroundSize="cover"
@@ -93,7 +93,7 @@ export default function SliderSection() {
                   }}
                   fontWeight={700}
                 >
-                  November 16's New Marvel
+                  Marvel Studios' Doctor Strange
                 </Heading>
                 <Text
                   fontSize={{
@@ -104,7 +104,55 @@ export default function SliderSection() {
                   fontWeight={600}
                   mb={10}
                 >
-                  This week's Marvel comics are filled with new beginnings.
+                  Enter a new dimension of Strange. Watch the official trailer
+                  for Marvel Studios'
+                </Text>
+              </VStack>
+            </VStack>
+          </VStack>
+        </Box>
+        <Box>
+          <VStack
+            position={"relative"}
+            h="480px"
+            w="full"
+            justifyContent="center"
+            color="white"
+            backgroundImage={
+              "url('https://cdn.britannica.com/62/182362-050-BD31B42D/Scarlett-Johansson-Black-Widow-Chris-Hemsworth-Thor.jpg')"
+            }
+            backgroundPosition="center"
+            backgroundSize="cover"
+          >
+            <Box position={"absolute"} w="full" h="full" left={0} top={0} />
+            <VStack
+              position={"absolute"}
+              zIndex={9}
+              w="80%"
+              alignItems={"flex-start"}
+              spacing={8}
+            >
+              <VStack alignItems={"flex-start"}>
+                <Heading
+                  fontSize={{
+                    sm: 20,
+                    lg: 30,
+                    "2xl": 40,
+                  }}
+                  fontWeight={700}
+                >
+                  Marvel & Moon Knight (2022)
+                </Heading>
+                <Text
+                  fontSize={{
+                    sm: 16,
+                    lg: 20,
+                    "2xl": 24,
+                  }}
+                  fontWeight={600}
+                  mb={10}
+                >
+                  THE FIST OF KHONSHU MEETS THE EMBIGGENED FIST OF MS.
                 </Text>
               </VStack>
             </VStack>
